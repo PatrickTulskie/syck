3ba201399a86f4a1b7b34dcc2229f2c1d6c95729
Patrick Tulskie <patricktulskie@gmail.com>
2014-05-05 14:29:07 -0400

    Don't define to_yaml on everything if we're already using Psych or something else already defined to_yaml.

--- lib/yaml/syck.rb
+++ lib/yaml/syck.rb
@@ -10,5 +10,6 @@ require 'syck/syck'
 require 'syck/tag'
 require 'syck/stream'
 require 'syck/constants'
-require 'syck/rubytypes'
+# Don't load all of the to_yaml stuff if something else already did...
+require 'syck/rubytypes' unless Object.respond_to?(:to_yaml)
 require 'syck/types'
