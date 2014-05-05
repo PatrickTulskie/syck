7879009d79df690dda65bad97e3104c1c6896c22
Patrick Tulskie <patricktulskie@gmail.com>
2014-05-05 15:15:00 -0400

    Skip loading syck/types as well to prevent to_yaml conflicts.

--- lib/yaml/syck.rb
+++ lib/yaml/syck.rb
@@ -12,4 +12,4 @@ require 'syck/stream'
 require 'syck/constants'
 # Don't load all of the to_yaml stuff if something else already did...
 require 'syck/rubytypes' unless Object.respond_to?(:to_yaml)
-require 'syck/types'
+require 'syck/types' unless Object.respond_to?(:to_yaml)
